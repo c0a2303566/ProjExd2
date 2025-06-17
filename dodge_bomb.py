60a89978f80d3f1699704a2ece695aa458ab1a21
nohi <c0a2303566@edu.teu.ac.jp>
2025-06-17 13:45:06 +0900

    DELTA

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -5,6 +5,12 @@ import pygame as pg
 
 WIDTH, HEIGHT = 1100, 650
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
+DELTA = {pg.K_UP: (0,-5),pg.K_DOWN: (0,+5),pg.K_LEFT:(-5,0),pg.K_RIGHT:(+5,0)}
+
+
+
+
+
 
 
 def main():
@@ -23,16 +29,21 @@ def main():
         screen.blit(bg_img, [0, 0]) 
 
         key_lst = pg.key.get_pressed()
-        sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
-        kk_rct.move_ip(sum_mv)
+        mv_x = mv_y = 0
+        for key, (dx,dy)in DELTA.items():
+            if key_lst[key]:
+                mv_x += dx
+                mv_y += dy
+                
+        # if key_lst[pg.K_UP]:
+        #     sum_mv[1] -= 5
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv[1] += 5
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv[0] -= 5
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv[0] += 5
+        kk_rct.move_ip(mv_x,mv_y)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
         tmr += 1
